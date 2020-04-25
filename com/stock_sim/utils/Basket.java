0aff377897c13262ac2fac176ca8084c053b48f2
Julien <julien.auclair93@gmail.com>
2020-04-25 18:21:12 +0200

    pb

--- com/stock_sim/utils/Basket.java
+++ com/stock_sim/utils/Basket.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:com/stock_sim/utils/Basket.java
 package com.stock_sim.utils;
 
 import java.util.ArrayList;
@@ -69,74 +68,4 @@ public class Basket {
         this.balance -= item.getPrice();
     }
 
-=======
-import java.util.ArrayList;
-
-/**
- * Basket
- */
-public class Basket {
-    private ArrayList<Item> basket;
-    private Double balance;
-    private Boolean payed;
-
-    public Basket() {
-        this.basket = new ArrayList<Item>();
-        this.balance = 0.0;
-        this.payed = false;
-    }
-
-    /**
-     * @return the basket
-     */
-    public ArrayList<Item> getBasket() {
-        return basket;
-    }
-
-    /**
-     * @param basket the basket to set
-     */
-    public void setBasket(ArrayList<Item> basket) {
-        this.basket = basket;
-    }
-
-    /**
-     * @return the balance
-     */
-    public Double getBalance() {
-        return balance;
-    }
-
-    /**
-     * @param balance the balance to set
-     */
-    public void setBalance(Double balance) {
-        this.balance = balance;
-    }
-
-    /**
-     * @return the payed
-     */
-    public Boolean getPayed() {
-        return payed;
-    }
-
-    /**
-     * @param payed the payed to set
-     */
-    public void setPayed(Boolean payed) {
-        this.payed = payed;
-    }
-
-    public void addItem(Item item) {
-        this.basket.add(item);
-        this.balance += item.getPrice();
-    }
-
-    public void removeItem(Item item) {
-        this.basket.remove(item);
-        this.balance -= item.getPrice();
-    }
-
->>>>>>> 9a5b25c4422d4de755cdd880d09b068e14c8a2fc:Basket.java
 }
